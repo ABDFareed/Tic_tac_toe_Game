93f1f1a9b6c82a00a500213ad215a9cabcd26719
ABDFareed <fareedyt01@gmail.com>
2025-03-16 03:58:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from "./component/Card/Card";
+import Grid from './component/Grid/Grid';
+
+function App(){
+  return (
+    <>
+      <Grid numberOfCards={9} />
+    </>
+  )
+}
+
+export default App;
